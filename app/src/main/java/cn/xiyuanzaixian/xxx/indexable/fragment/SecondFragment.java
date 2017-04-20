9b28457a45ef049a3059d1e1931d64e6efe47056
zhouchong <zhouchong741@hotmail.com>
2017-04-20 13:12:56 +0800

    处理 fragment 中点击 edittext 底部导航栏被顶起

--- app/src/main/java/cn/xiyuanzaixian/xxx/indexable/fragment/SecondFragment.java
+++ app/src/main/java/cn/xiyuanzaixian/xxx/indexable/fragment/SecondFragment.java
@@ -2,10 +2,15 @@ package cn.xiyuanzaixian.xxx.indexable.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.blankj.aloglibrary.ALog;
 
 import cn.xiyuanzaixian.xxx.indexable.R;
 
@@ -14,6 +19,8 @@ import cn.xiyuanzaixian.xxx.indexable.R;
  */
 public class SecondFragment extends Fragment {
 
+
+    private EditText editText;
 
     public static SecondFragment newInstance() {
         // Required empty public constructor
@@ -28,4 +35,22 @@ public class SecondFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        editText = (EditText) getActivity().findViewById(R.id.edit_delete);
+        ImageButton delete_btn = (ImageButton) getActivity().findViewById(R.id.delete_btn);
+
+        delete_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText == null){
+                    ALog.d("Nothing");
+                }else {
+                    editText.setText("");
+                }
+            }
+        });
+    }
 }
